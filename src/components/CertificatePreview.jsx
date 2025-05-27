1fa7214f9b2185a821c4656d16c9fd498a996365
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2025-05-27 20:22:42 +0530

    from lok..Nav and Main pushed

---
+++ src/components/CertificatePreview.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CertificatePreview = () => {
+  return (
+    <div>CertificatePreview</div>
+  )
+}
+
+export default CertificatePreview
---
+++ src/components/layouts/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header className="flex items-center justify-between p-4 shadow-md bg-white">
+        <div className="text-xl font-bold">Certify</div>
+        <nav className="hidden md:flex gap-6 text-gray-700">
+          <a href="#" className="hover:text-black">
+            Browse
+          </a>
+          <a href="#" className="hover:text-black">
+            For Enterprise
+          </a>
+          <a href="#" className="hover:text-black">
+            Pricing
+          </a>
+          <a href="#" className="hover:text-black">
+            Support
+          </a>
+        </nav>
+        <div className="flex gap-2">
+          <button className="px-4 py-2 bg-orange-600 text-white rounded-lg hover:bg-orange-500 cursor-pointer  ">
+            Sign up
+          </button>
+          <button className="px-4 py-2 border rounded text-gray-700 hover:bg-orange-200 cursor-pointer">
+            Log in
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
